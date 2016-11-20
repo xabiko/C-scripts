097d22f6a034d5b7ac62761dadcb00aeb3f13a87
xabiko <jdejprado@gmail.com>
2016-11-20 15:30:21 -0700

    insertion-sort added

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Collection of C++ scripts:
+6. insertion-sort : O(n^2) sorting algorithm.
 5. bitshift.cpp : simple iteration over bit-shifted integers.
 4. basicObject.cpp : basic object with set & get functions.
 3. refClass.cpp : passing reference to function call, instead of entire object.
---
+++ insertion-sort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void insertion_sort(vector<int>& vector){
+
+	for(int j=1; j<=vector.size()-1; j+=1){
+
+		auto key = vector[j];
+		int i   = j - 1;
+		while(i>=0 && vector[i]>key){
+			vector[i+1] = vector[i];
+			i-=1;
+		}
+
+	vector[i+1] = key;
+	}
+}
+
+int main(){
+
+	int  n;
+
+	cout << "Input the number of integers to be sorted:" << endl;
+	cin  >> n;
+
+	vector<int> v(n);
+
+	cout << "Intput desired elements:" << endl;
+	for(int& i : v){
+		cin >> i;
+	}
+	cout << "Ordered elements:" << endl;
+	insertion_sort(v);
+	for(int& i : v){
+		cout << i << ",";
+	}
+}
