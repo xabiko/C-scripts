f695b578b19a3ff3bd2d3b908012d8b7101418b4
xabiko <jdejprado@gmail.com>
2016-11-24 00:00:08 -0700

    merge sort added

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Collection of C++ scripts:  
-
-6. insertion-sort : O(n^2) sorting algorithm.
-5. bitshift.cpp : simple iteration over bit-shifted integers.
-4. basicObject.cpp : basic object with set & get functions.
-3. refClass.cpp : passing reference to function call, instead of entire object.
-2. ref.cpp : modifying global variable from called function, using a reference.
-1. swap.cpp : swap algorithm in 1 line, using the XOR bit-logic operator.
+# Collection of C++ scripts:
+* merge-sort: O(nlogn) sorting algorithm, implementing infinity sentinel.
+* insertion-sort : O(n^2) sorting algorithm.
+* bitshift.cpp : simple iteration over bit-shifted integers.
+* basicObject.cpp : basic object with set & get functions.
+* refClass.cpp : passing reference to function call, instead of entire object.
+* ref.cpp : modifying global variable from called function, using a reference.
+* swap.cpp : swap algorithm in 1 line, using the XOR bit-logic operator.
---
+++ merge-sort.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+#include<limits>
+using namespace std;
+
+void merge_(vector<int>& vec, int p, int q, int r){
+
+	int n1 = q-p+1;
+	int n2 = r-q;
+	int left[n1+1],right[n2+1];
+
+	for(int i=0; i<n1; i++){
+		left[i] = vec[p+i];
+	}
+	for(int i=0; i<n2; i++){
+		right[i] = vec[q+i+1];
+	}
+	left[n1]=numeric_limits<int>::max();
+	right[n2]=numeric_limits<int>::max();
+
+	int i=0, j=0;
+	for(int k=p; k<=r; k++){
+		if(left[i]<=right[j]){
+		vec[k] = left[i];
+		i++;
+		}
+		else{
+		vec[k] = right[j];
+		j++;
+		}
+	}
+}
+
+void merge_sort(vector<int>& vec, int p, int r){
+	if(p<r){
+		int q = p+(r-p)/2;
+		merge_sort(vec,p,q);
+		merge_sort(vec,q+1,r);
+		merge_(vec,p,q,r);
+	}
+}
+
+int main(){
+
+	int  n;
+
+	cout << "Input the number of integers to be sorted:" << endl;
+	cin  >> n;
+
+	vector<int> v(n);
+
+	cout << "Intput desired elements:" << endl;
+	for(auto& i : v){
+		cin >> i;
+	}
+	cout << "Ordered elements:" << endl;
+	merge_sort(v,0,v.size()-1);
+	for(auto& i : v){
+		cout << i << ",";
+	}
+}
